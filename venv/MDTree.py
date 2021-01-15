1cdf22876a21ce2bbc277c74b764ae3ac6959828
starriest <starriest30@gmail.com>
2021-01-16 00:34:18 +0900

    fix parsertree bug

--- venv/MDTree.py
+++ venv/MDTree.py
@@ -1,3 +1,5 @@
+import re
+
 HEADER = "#"
 INDENT = ">"
 
@@ -15,9 +17,25 @@ UNORDERED_LIST_PLUS = "+"
 BLOCK_CODE = "```"
 INLINE_CODE ="`"
 
+TITLE_REGEX = re.compile("^[#].* ")
+INLINE_LINK_REGEX = re.compile("\[[가-힣A-Za-z].*\]\(.*\)")
+HREF_REGEX = re.compile("http:\/\/[가-힣A-Za-z].*[A-Za-z]")
+LINK_INFO_REGEX = re.compile("\[[가-힣A-Za-z].\]")
+ITALIC_REGEX = re.compile("\*.*\*")
+BOLD_REGEX = re.compile("\*\*.*\*\*")
+INLINE_CODE_REGEX = re.compile("\`.*\`")
+HIGHLIGHT_REGEX = re.compile("==.*==")
+IMAGE_REGEX = re.compile("!\[.*\]\(.*\)")
+IMAGE_INFO_REGEX = re.compile("\[.*\]")
+IMAGE_URI_REGEX = re.compile("\(.*\)")
+
+types = ['heading', 'character', 'image', 'blockquote', 'code_block', 'code_inline', 'nonetype']
+
 class Node:
     def __init__(self, char):
         self.char = char
+        self.type = 'nonetype'
+
         self.parent = None
         self.childs = list()
 
@@ -40,31 +58,30 @@ class Node:
     def get_char(self):
         return self.char
 
+    def set_type(self, type):
+        self.type = type
+
 class MarkdownParser:
     def __init__(self):
-        self.root = Node("")
+        self.root = Node("p")
         self.node = self.root
 
+    idx = 0
     def parse(self, lines):
-        for ch in lines:
-            child = Node(ch)
-            self.node.add_child(child)
-            
-            if ch in [HEADER, INDENT]:
-                self.node = child
+        line = lines[0]
+        idx = 0
+        while True:
+            g = TITLE_REGEX.search(line.lstrip())
+            if type(g) != type(None):
+                self.node.set_type('heading')
+                line = re.sub(TITLE_REGEX, line)
+                child = Node(line)
+                self.node.add_child(child)
+                continue
 
-            # elif ch is BLOCK_CODE:
-            #     if child.get_parent().get_char() is BLOCK_CODE:
-            #         self.node = child.get_parent()
-            #     else:
-            #         self.node = child
-
-            elif ch is NEW_LINE:
-                if child.get_parent().get_char() is BLOCK_CODE:
-                    continue
-                elif child.get_parent() is not None:
-                    self.node = child.get_parent()
-            
+            else:
+                idx += 1
+                line = lines[idx]
 
     def _to_string(self):
         to_string(self.root)
@@ -78,24 +95,26 @@ def to_html(node):
     for child in childs:
         ch = child.get_char()
         
-        if ch in [HEADER, INDENT] :
+        if ch is HEADER:
             print("<h1>",end="")
             to_html(child)
             print("</h1>")
-        elif ch is BLOCK_CODE:
-            if child.get_parent().get_char() is BLOCK_CODE:
-                return 
-            else:
-                print("<code><pre>")
-                to_string(child)
-                print("</pre></code>")
+
+        elif ch is INDENT:
+            print("<blockquote>",end="")
+            to_html(child)
+            print("</blockquote>")
+        # elif ch is BLOCK_CODE:
+        #     if child.get_parent().get_char() is BLOCK_CODE:
+        #         return 
+        #     else:
+        #         # print("<code><pre>")
+        #         to_string(child)
+        #         # print("</pre></code>")
 
         elif ch is NEW_LINE:
-            if child.get_parent().get_char() is BLOCK_CODE:
-                continue
-            else:
-                print("</p>")
-                return
+            print("</p>", end="")
+            return
         else:
             print(ch, end="")
             
@@ -111,7 +130,7 @@ def to_string(node):
             to_string(child)
 
 
-line = " # 안녕하세요\n # 반가워요 \n\n 잘있어요 \n\n > 다시 만나요\n```Java\n int a = 1 \n ```\n # 헤딩"
+line = " # 안녕하세요\n # 감사해요\n > 이건 블록 주석 \n >> 블록주석2 \n"
 parser = MarkdownParser()
 
 print("===== parse =====")
@@ -119,6 +138,10 @@ parser.parse(line)
 
 print("===== debug =====")
 parser._to_html()
+print()
+
+# print("===== debug =====")
+# parser._to_string()
 
 
 
